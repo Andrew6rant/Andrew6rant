e78c4ac418f6e95f465aa4d17985a1e3d8212bd2
Andrew Grant <andrew.grant@mail.rit.edu>
2022-02-27 20:39:00 -0500

    optimize global variables
    I truly have no idea why I made so many in the first place

--- today.py
+++ today.py
@@ -4,19 +4,15 @@ import requests
 import os
 from xml.dom import minidom
 
-date1 = datetime.datetime(2002, 7, 5)
-now = datetime.datetime.today()
-date2 = datetime.datetime(now.year, now.month, now.day)
-
-diff = relativedelta.relativedelta(date2, date1)
-
-years = diff.years
-months = diff.months
-days = diff.days
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 
 
 def daily_readme():
-    return '{} {}, {} {}, {} {}'.format(years, 'year' + format_plural(years), months, 'month' + format_plural(months), days, 'day' + format_plural(days))
+    birth = datetime.datetime(2002, 7, 5)
+    now = datetime.datetime.today()
+    diff = relativedelta.relativedelta(now, birth)
+    
+    return '{} {}, {} {}, {} {}'.format(diff.years, 'year' + format_plural(diff.years), diff.months, 'month' + format_plural(diff.months), diff.days, 'day' + format_plural(diff.days))
 
 
 def format_plural(unit):
@@ -25,8 +21,17 @@ def format_plural(unit):
     return ''
 
 
-def graph_ql(query, start_date, end_date):
-    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+def graph_commits(start_date, end_date):
+    query = '''
+    query($start_date: DateTime!, $end_date: DateTime!) {
+        user(login: "Andrew6rant") {
+            contributionsCollection(from: $start_date, to: $end_date) {
+                contributionCalendar {
+                    totalContributions
+                }
+            }
+        }
+    }'''
     variables = {'start_date': start_date,'end_date': end_date}
     headers = {'authorization': 'token '+ ACCESS_TOKEN}
     request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables':variables}, headers=headers)
@@ -40,27 +45,17 @@ def svg_overwrite(filename):
     f = open(filename, mode='w', encoding='utf-8')
     tspan = svg.getElementsByTagName('tspan')
     tspan[30].firstChild.data = daily_readme()
-    tspan[66].firstChild.data = f'{commit_counter(now): <12}'
+    tspan[66].firstChild.data = f'{commit_counter(datetime.datetime.today()): <12}'
     f.write(svg.toxml("utf-8").decode("utf-8"))
 
 
 def commit_counter(date):
-    query = '''
-    query($start_date: DateTime!, $end_date: DateTime!) {
-        user(login: "Andrew6rant") {
-            contributionsCollection(from: $start_date, to: $end_date) {
-                contributionCalendar {
-                    totalContributions
-                }
-            }
-        }
-    }'''
     total_commits = 0
     # since GraphQL's contributionsCollection has a maximum reach of one year
     while date.isoformat() > "2019-11-02T00:00:00.000Z": # one day before my very first commit
         old_date = date.isoformat()
         date = date - datetime.timedelta(days=365)
-        total_commits += graph_ql(query, date.isoformat(), old_date)
+        total_commits += graph_commits(date.isoformat(), old_date)
     return total_commits
 
 
